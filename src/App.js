67ff4a6f982041c5fd01166faf3fec280664c647
ric1709 <abbaskanov@gmail.com>
2022-03-22 12:19:45 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
       <h1 onClick={()=>console.log('test')}>Baiaaly</h1>
     </div>
   );
